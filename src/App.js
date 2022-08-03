6fe762657a88d4feaa93a2299cc2e2cfb106dbf6
Inarm <mramrasansut@outlook.co.th>
2022-08-03 21:05:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get('https://api.publicapis.org/categories')
+      .then(res => {
+        setData(res.data.categories)
+      })
+      .catch(err => {
+        console.log("Error", err)
+        return null
+      })
+  }, []);
+
+  const SearchCategories = (e) => {
+    if (e.key === 'Enter') {
+      setSearch(e.target.value)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container' style={{ display: "flex" }}>
+      <table>
+        <tr>
+          <th> Categories </th>
+        </tr>
+        {data.map((x) => <tr key={x}>{x}</tr>)}
+      </table>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => SearchCategories(e)}
+          placeholder="Enter categories"
+          onKeyPress={SearchCategories}
+          style={{ height: 50 }}
+        />
+        { data.includes(search) === true ? <p>{search}</p> : <p>Try to search again</p>}
+      </div>
     </div>
   );
 }
